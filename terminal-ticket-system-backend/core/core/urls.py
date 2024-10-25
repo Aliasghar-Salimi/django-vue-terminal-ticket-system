7602ad872239f8679c5a870639d76eff8c86f1cb
Aliasghar-salimi <aliasghar.salimi.05@gmail.com>
2024-10-25 19:52:50 +0330

    swagger setup completed

--- terminal-ticket-system-backend/core/core/urls.py
+++ terminal-ticket-system-backend/core/core/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # swagger
+    path('api-schema/', get_schema_view(title='API schema'), name='api_schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='docs.html', extra_context={'schema_url':'api_schema'},)),
+    
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('cooperatives.urls')),
     path('api/v1/auth/', include('djoser.urls')),
